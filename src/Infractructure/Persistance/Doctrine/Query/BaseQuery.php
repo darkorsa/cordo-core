c3f9e888102878eebf1d3918df08e9cc4f3011c1
darkorsa <dkorsak@gmail.com>
2021-09-19 22:15:20 +0200

    BaseQuery improvements

--- src/Infractructure/Persistance/Doctrine/Query/BaseQuery.php
+++ src/Infractructure/Persistance/Doctrine/Query/BaseQuery.php
@@ -6,6 +6,7 @@ namespace Cordo\Core\Infractructure\Persistance\Doctrine\Query;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Query\QueryBuilder;
+use Doctrine\Common\Collections\ArrayCollection;
 use Cordo\Core\Application\Exception\ResourceNotFoundException;
 
 abstract class BaseQuery
@@ -26,14 +27,14 @@ abstract class BaseQuery
     {
         $this->filter($filter, $queryBuilder);
 
-        return (string) $this->connection->fetchColumn($queryBuilder->getSQL(), $queryBuilder->getParameters());
+        return (string) $this->connection->fetchOne($queryBuilder->getSQL(), $queryBuilder->getParameters());
     }
 
     protected function assoc(QueryBuilder $queryBuilder, ?QueryBuilderFilter $filter = null): array
     {
         $this->filter($filter, $queryBuilder);
 
-        $return = $this->connection->fetchAssoc($queryBuilder->getSQL(), $queryBuilder->getParameters());
+        $return = $this->connection->fetchAssociative($queryBuilder->getSQL(), $queryBuilder->getParameters());
 
         if (!$return) {
             throw new ResourceNotFoundException("Cannot get resource");
@@ -46,7 +47,18 @@ abstract class BaseQuery
     {
         $this->filter($filter, $queryBuilder);
 
-        return $this->connection->fetchAll($queryBuilder->getSQL(), $queryBuilder->getParameters());
+        return $this->connection->fetchAllAssociative($queryBuilder->getSQL(), $queryBuilder->getParameters());
+    }
+
+    protected function createCollection(array $result, string $view): ArrayCollection
+    {
+        $collection = new ArrayCollection();
+        array_map(static function (array $data) use ($collection, $view) {
+            // @phpstan-ignore-next-line
+            $collection->add(call_user_func($view . '::fromArray', $data));
+        }, $result);
+
+        return $collection;
     }
 
     protected function filter(?QueryBuilderFilter $queryFilter, QueryBuilder $queryBuilder)
