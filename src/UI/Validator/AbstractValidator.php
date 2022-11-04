3488f8c4074258cebb5868de3aba70cb84c9d40f
darkorsa <dkorsak@gmail.com>
2022-11-04 15:36:43 +0100

    AbstractValidator docblock

--- src/UI/Validator/AbstractValidator.php
+++ src/UI/Validator/AbstractValidator.php
@@ -8,6 +8,23 @@ use Cordo\Core\Application\App;
 use Illuminate\Validation\Factory;
 use Illuminate\Validation\Validator;
 
+/**
+ * @method bool passes()
+ * @method bool fails()
+ * @method array validate()
+ * @method array validated()
+ * @method array valid()
+ * @method array invalid()
+ * @method \Illuminate\Support\MessageBag messages()
+ * @method \Illuminate\Support\MessageBag errors()
+ * @method array attributes()
+ * @method \Illuminate\Validation\Validator setData(array $data)
+ * @method array getData()
+ * @method mixed getValue()
+ * @method array getRules()
+ * @method \Illuminate\Validation\Validator setRules(array $rules)
+ * @method \Illuminate\Validation\Validator sometimes($attribute, $rules, callable $callback)
+ */
 abstract class AbstractValidator
 {
     protected Validator $validator;
